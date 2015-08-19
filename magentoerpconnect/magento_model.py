2c61d842571f2013ceed986c7d22ba386d782299
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-08-19 16:43:49 -0300

    [FIX] #129 - Wrap synchonize metadata method and give the user a warning

--- magentoerpconnect/magento_model.py
+++ magentoerpconnect/magento_model.py
@@ -23,6 +23,7 @@
 import logging
 from datetime import datetime, timedelta
 from openerp import models, fields, api, _
+from openerp.exceptions import Warning
 from openerp.addons.connector.session import ConnectorSession
 from openerp.addons.connector.connector import ConnectorUnit
 from openerp.addons.connector.unit.mapper import mapping, ImportMapper
@@ -202,32 +203,38 @@ class MagentoBackend(models.Model):
 
     @api.multi
     def synchronize_metadata(self):
-        mag_tax_class_obj = self.env['magento.tax.class']
-        default_tax_list = [
-            {'name': 'default', 'magento_id': 1},
-            {'name': 'Taxable Goods', 'magento_id': 2},
-            {'name': 'Shipping', 'magento_id': 4},
-            ]
-        session = ConnectorSession(self.env.cr, self.env.uid,
-                                   context=self.env.context)
-        for backend in self:
-            for model in ('magento.website',
-                          'magento.store',
-                          'magento.storeview',
-                          'magento.attribute.set'):
-                # import directly, do not delay because this
-                # is a fast operation, a direct return is fine
-                # and it is simpler to import them sequentially
-                import_batch(session, model, backend.id)
-
-            # create magento.tax.class records as we don't import them
-            if not backend.tax_imported:
-                for tax_dict in default_tax_list:
-                    tax_dict.update(backend_id=backend.id)
-                    mag_tax_class_obj.create(tax_dict)
-                backend.tax_imported = True
-
-        return True
+        try:
+            mag_tax_class_obj = self.env['magento.tax.class']
+            default_tax_list = [
+                {'name': 'default', 'magento_id': 1},
+                {'name': 'Taxable Goods', 'magento_id': 2},
+                {'name': 'Shipping', 'magento_id': 4},
+                ]
+            session = ConnectorSession(self.env.cr, self.env.uid,
+                                       context=self.env.context)
+            for backend in self:
+                for model in ('magento.website',
+                              'magento.store',
+                              'magento.storeview',
+                              'magento.attribute.set'):
+                    # import directly, do not delay because this
+                    # is a fast operation, a direct return is fine
+                    # and it is simpler to import them sequentially
+                    import_batch(session, model, backend.id)
+    
+                # create magento.tax.class records as we don't import them
+                if not backend.tax_imported:
+                    for tax_dict in default_tax_list:
+                        tax_dict.update(backend_id=backend.id)
+                        mag_tax_class_obj.create(tax_dict)
+                    backend.tax_imported = True
+    
+            return True
+        except Exception as e:
+            _logger.error(e.message, exc_info=True)
+            raise Warning(
+                _(u"Check your configuration, we can't get the data.\
+                Here is the error:\n%s") % str(e).decode('utf-8', 'ignore'))
 
     @api.multi
     def import_partners(self):
