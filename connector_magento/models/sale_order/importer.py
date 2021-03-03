7df6623b77d7dbaabbb40dc89dd70b8946b4a6be
Wolfgang Pichler <wpichler@callino.at>
2021-03-03 17:19:28 +0100

    [FIX] Do use positive quantity and negative amount for gift line

--- connector_magento/models/sale_order/importer.py
+++ connector_magento/models/sale_order/importer.py
@@ -218,9 +218,9 @@ class SaleOrderImportMapper(Component):
             ], limit=1)
         line = {
             'product_id': self.backend_record.default_gift_product_id.id,
-            'price_unit': amount,
+            'price_unit': amount * -1,
             'name': name,
-            'product_uom_qty': -1,
+            'product_uom_qty': 1,
             'tax_id': [(4, tax_id)] if tax_id else None,
         }
         values['order_line'].append((0, 0, line))
